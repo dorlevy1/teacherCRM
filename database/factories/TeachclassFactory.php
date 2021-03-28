4ef28488d29fc20cc7a3c60c697610c0ce1c6378
dor <ddl105095@gmail.com>
2021-03-29 02:04:58 +0300

    first commit

---
+++ database/factories/TeachclassFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Teachclass;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TeachclassFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Teachclass::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
