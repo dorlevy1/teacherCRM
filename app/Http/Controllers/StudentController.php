696a9cdc86cd743fa00ddbfe419ef025b981a1c2
Dor Levy <dor@intername.media>
2021-04-03 11:34:57 +0300

    add Controllers and Routes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(Teacher $teacher){
+
+        return view('student.index',compact($teacher->students));
+    }
+
+    public function create(){}
+
+    public function edit(){}
+
+    public function update(){}
+
+    public function destroy(){}
 }
